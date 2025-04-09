29ec383025461f69ec415814c573749a953128c5
yangjiandong <chunquedong@163.com>
2025-04-09 20:05:39 +0800

    sric support

---
+++ build.sh
@@ -0,0 +1,5 @@
+
+fan fmake libjsonc.props
+fan fmake libjsonc.props -debug
+
+sric sric/module.scm
---
+++ sric/test.scm
@@ -0,0 +1,7 @@
+name = jsoncSricTest
+summary = json lib test
+outType = exe
+version = 1.0
+depends = sric 1.0, jsonc 1.0
+srcDirs = ./test.sric
+license = GNU LESSER GENERAL PUBLIC LICENSE version 3.0
